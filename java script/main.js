9cb0743bee307b26c7f0b2c8ea7a62922e58832e
megabebrik <113171956+megabebrik@users.noreply.github.com>
2022-12-15 10:03:02 +0600

    Add files via upload

---
+++ java script/main.js
@@ -0,0 +1,46 @@
+console.log("aaaaa")
+let str='decdsccdwdc'
+console.log(str)
+let a,b
+a = 2
+b = 678
+console.log (a+b)
+function sum(a,b){
+    return console.log(a-b)
+}
+sum(7,8) 
+
+function vpd (n){
+    return console.log('Привет '+ n)
+}
+vpd('Vladislav Petrenko')
+if(a>=3){
+    console.log('ssswesz')
+}
+else{
+     console.log('345')   
+}
+console.log(5%2)
+function sur(v){
+    
+}
+let div = document.querySelectorAll('.jaba')
+console.log(div)
+div[0].innerHTML = 'suka'
+let loks = document.querySelectorAll('.bebra2')
+console.log(loks[0])
+
+function bebra3(){
+    console.log('Тебя скелет поцеловал')
+    div[0].innerHTML = a1++
+}
+
+let a1=0
+
+let loks2 = document.querySelectorAll('.bebra')
+console.log(loks2[0])
+
+function bebra4(){
+    console.log('Тебя скелет поцеловал2')
+    div[0].innerHTML = a1--
+}
